8c81eb69b05a8e115c062fba387907d8820aee62
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 00:56:06 +0200

    Add files via upload

---
+++ APP/led_blink_exti.c
@@ -0,0 +1,28 @@
+#include <xc.h>
+#define _XTAL_FREQ 8000000
+
+#include "HAL/LED/LED_INTERFACE.h"
+#include "MCAL/EXTI/EXTI_Interface.h"
+
+/* LED Definition */
+#define LED_PORT   GPIO_PORTB
+#define LED_PIN    GPIO_PIN1
+
+void Toggle_LED(void)
+{
+    LED_Toggle(LED_PORT, LED_PIN);
+}
+
+void main(void)
+{
+    LED_Init(LED_PORT, LED_PIN);
+
+    EXTI_Init();
+    EXTI_SetCallBack(Toggle_LED);
+    EXTI_Enable();
+
+    while(1)
+    {
+        /* Main loop empty */
+    }
+}
