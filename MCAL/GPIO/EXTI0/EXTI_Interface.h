7b7a88dc55ae99675a80bd7f53d3586ccc30d003
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 00:58:36 +0200

    Update EXTI_Interface.h

---
+++ MCAL/GPIO/EXTI0/EXTI_Interface.h
@@ -0,0 +1,16 @@
+#ifndef EXTI_INTERFACE_H
+#define EXTI_INTERFACE_H
+
+#include "../../SERVICES/STD_TYPES.h"
+
+
+#define EXTI_RISING_EDGE    0
+#define EXTI_FALLING_EDGE   1
+
+void EXTI_Init(void);
+void EXTI_SetEdge(u8 Edge);
+void EXTI_Enable(void);
+void EXTI_Disable(void);
+void EXTI_SetCallBack(void (*Copy_ptr)(void));
+
+#endif
