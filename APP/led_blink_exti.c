d669055ea08a17a1555e9b6c931a3cf71c9cd8d3
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 09:17:21 +0200

    Refactor LED blinking code for clarity and structure

--- APP/led_blink_exti.c
+++ APP/led_blink_exti.c
@@ -1,31 +1,30 @@
-#include <xc.h>
-#define _XTAL_FREQ 8000000
+#include "../MCAL/GPIO/GPIO_Interface.h"
+#include "../MCAL/EXTI0/EXTI_Interface.h"
+#include "../HAL/LED/LED_Interface.h"
 
-#include "../HAL/LED/LED_interface.h"
-#include "../MCAL/GPIO/GPIO_interface.h"
-#include "../MCAL/GPIO/EXTI0/EXTI_Interface.h"
+/* LED connected to PORTB, PIN1 */
+#define LED_PORT    GPIO_PORTB
+#define LED_PIN     GPIO_PIN1
 
-/* LED Definition */
-#define LED_PORT   GPIO_PORTB
-#define LED_PIN    GPIO_PIN1
-
+/* Callback function - toggles LED */
 void Toggle_LED(void)
 {
     LED_Toggle(LED_PORT, LED_PIN);
 }
 
-void main(void)
+void main()
 {
+
     LED_Init(LED_PORT, LED_PIN);
 
+    /* Initialize EXTI */
     EXTI_Init();
     EXTI_SetCallBack(Toggle_LED);
     EXTI_Enable();
 
+
     while(1)
     {
-        /* Main loop empty */
+
     }
-
 }
-
