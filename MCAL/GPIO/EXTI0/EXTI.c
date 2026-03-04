b1ea112de651bda7db95c4dc752effdf81d92468
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 09:16:11 +0200

    Refactor EXTI.c for improved readability and structure

--- MCAL/GPIO/EXTI0/EXTI.c
+++ MCAL/GPIO/EXTI0/EXTI.c
@@ -1,42 +1,50 @@
-#include "EXTI_Interface.h"
-#include "EXTI_Config.h"
 #include "EXTI_Private.h"
+#include "EXTI_Config.h"
+#include "EXTI_Interface.h"
+#include "../../SERVICES/BIT_MATH.h"
 
 static void (*EXTI_CallBack)(void) = 0;
 
 void EXTI_Init(void)
 {
-    /* Set RB0 as input */
-    TRISB0 = 1;
 
-    /* Set Edge */
-    EXTI_SetEdge(EXTI_EDGE);
+    TRISB.B0 = 1;
 
-    /* Clear interrupt flag */
-    INTF = 0;
+
+    #if EXTI_EDGE == EXTI_RISING_EDGE
+
+        OPTION_REG.INTEDG = 1;
+    #else
+
+        OPTION_REG.INTEDG = 0;
+    #endif
+
+
+    CLR_BIT(INTCON_REG, INTF_POS);
+
 }
 
 void EXTI_SetEdge(u8 Edge)
 {
     if (Edge == EXTI_RISING_EDGE)
     {
-        INTEDG = 1;
+        SET_BIT(OPTION_REG_ADDR, INTEDG_POS);
     }
     else if (Edge == EXTI_FALLING_EDGE)
     {
-        INTEDG = 0;
+        CLR_BIT(OPTION_REG_ADDR, INTEDG_POS);
     }
 }
 
 void EXTI_Enable(void)
 {
-    INTE = 1;   // Enable INT0
-    GIE  = 1;   // Enable Global Interrupt
+    SET_BIT(INTCON_REG, INTE_POS);
+    SET_BIT(INTCON_REG, GIE_POS);
 }
 
 void EXTI_Disable(void)
 {
-    INTE = 0;
+    CLR_BIT(INTCON_REG, INTE_POS);
 }
 
 void EXTI_SetCallBack(void (*Copy_ptr)(void))
@@ -44,16 +52,16 @@ void EXTI_SetCallBack(void (*Copy_ptr)(void))
     EXTI_CallBack = Copy_ptr;
 }
 
-/* ISR */
-void __interrupt() ISR(void)
+
+void interrupt()
 {
-    if (INTF == 1)
+    if (GET_BIT(INTCON_REG, INTF_POS))
     {
-        if (EXTI_CallBack != 0)
+        if (EXTI_CallBack != NULL_PTR)
         {
             EXTI_CallBack();
         }
 
-        INTF = 0;  // Clear flag
+        CLR_BIT(INTCON_REG, INTF_POS);
     }
 }
