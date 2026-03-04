1454851a4e880b01d0ac7b6311839fe7e4826f6d
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 09:16:56 +0200

    Refactor EXTI_Private.h with register addresses and positions

--- MCAL/GPIO/EXTI0/EXTI_Private.h
+++ MCAL/GPIO/EXTI0/EXTI_Private.h
@@ -1,14 +1,22 @@
 #ifndef EXTI_PRIVATE_H
 #define EXTI_PRIVATE_H
 
-#include <xc.h>
 
-/* INTCON Register Bits */
-#define GIE     7
-#define INTE    4
-#define INTF    1
 
-/* OPTION_REG Bit */
-#define INTEDG  6
+
+
+/* INTCON Register Address (from pic16f877.h) */
+#define INTCON_REG      (*(volatile u8*)0x0B)  // INTCON address
+
+/* INTCON Register Bit Positions */
+#define GIE_POS         7      // Global Interrupt Enable bit position
+#define INTE_POS        4      // INT0 External Interrupt Enable bit position
+#define INTF_POS        1      // INT0 External Interrupt Flag bit position
+
+/* OPTION_REG Register Address */
+#define OPTION_REG_ADDR (*(volatile u8*)0x81)  // OPTION_REG address (Bank 1)
+
+/* OPTION_REG Bit Positions */
+#define INTEDG_POS      6      // Interrupt Edge Select bit position
 
 #endif
