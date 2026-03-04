544e5c9f7a37ff59fe2b2eb8d1ae7a7cbf7bd2be
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2026-03-04 09:15:46 +0200

    Update EXTI_Config.h

--- MCAL/GPIO/EXTI0/EXTI_Config.h
+++ MCAL/GPIO/EXTI0/EXTI_Config.h
@@ -1,7 +1,7 @@
 #ifndef EXTI_CONFIG_H
 #define EXTI_CONFIG_H
 
-/* Choose interrupt edge */
+/* Choose interrupt edge: EXTI_RISING_EDGE or EXTI_FALLING_EDGE */
 #define EXTI_EDGE   EXTI_FALLING_EDGE
 
 #endif
